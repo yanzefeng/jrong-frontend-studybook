83a8cb88ff409ee97132dab2d7af955a0cbafee0
JRong <henry_88_lee@163.com>
2022-02-13 22:53:28 +0800

    【修改堆】

--- leetcode/1046. 最后一块石头的重量.js
+++ leetcode/1046. 最后一块石头的重量.js
@@ -2,13 +2,72 @@
  * @param {number[]} stones
  * @return {number}
  */
- var lastStoneWeight = function(stones) {
-  let arr = stones.sort((a, b) => b - a)
-  while(arr.length > 1) {
-      const y = arr.shift()
-      const x = arr.shift()
-      let h = y - x
-      if (h > 0) arr = [...arr, h].sort((a, b) => b - a)
+ class Heap {
+  constructor() {
+    this.data = []
   }
-  return arr
+  // 向下调
+  shiftDown(ind = 0) {
+    let n = this.size() - 1
+    while (ind * 2 + 1 <= n) {
+      let tmp = ind
+      if (this.data[tmp] < this.data[ind * 2 + 1]) tmp = ind * 2 + 1
+      if (ind * 2 + 2 <= n && this.data[tmp] < this.data[ind * 2 + 2])
+      tmp = ind * 2 + 2
+      if (tmp === ind) break
+      this.data[tmp] = [this.data[ind], (this.data[ind] = this.data[tmp])][0]
+      ind = tmp
+    }
+  }
+  // 向上调整
+  shiftUp(idx) {
+    let pIdx = null
+    while ((pIdx = Math.floor((idx - 1) / 2)), idx && this.data[pIdx] < this.data[idx]) {
+      this.data[pIdx] = [this.data[idx], (this.data[idx] = this.data[pIdx])][0]
+      idx = pIdx
+    }
+  }
+  push(val) {
+    this.data.push(val)
+    this.shiftUp(this.size() - 1)
+  }
+  pop() {
+    let res = this.top()
+    if (this.size() === 0) return
+    if (this.size() === 1) {
+        return this.data.pop()
+    }
+    this.data[0] = this.data.pop()
+    this.shiftDown(0)
+    return res
+  }
+  top() {
+    return this.data[0]
+  }
+  size() {
+      return this.data.length
+  }
+}
+var lastStoneWeight = function(stones) {
+    let h = new Heap()
+    for (let val of stones) {
+        h.push(val)
+    }
+    while(h.size() > 1) {
+        const y = h.pop()
+        const x = h.pop()
+        const diff = y - x
+        if (diff > 0) {
+            h.push(diff)
+        }
+    }
+    return h.data
+    // let arr = stones.sort((a, b) => b - a)
+    // while(arr.length > 1) {
+    //     const y = arr.shift()
+    //     const x = arr.shift()
+    //     let h = y - x
+    //     if (h > 0) arr = [...arr, h].sort((a, b) => b - a)
+    // }
+    // return arr
 };
--- leetcode/剑指 Offer 40. 最小的k个数.js
+++ leetcode/剑指 Offer 40. 最小的k个数.js
@@ -1,11 +1,60 @@
-
 /**
  * @param {number[]} arr
  * @param {number} k
  * @return {number[]}
  */
-var getLeastNumbers = function (arr, k) {
-  arr = arr.sort((a, b) => a - b)
-  arr = arr.slice(0, k)
-  return arr
+ class Heap {
+  constructor() {
+    this.data = []
+  }
+  // 向下调
+  shiftDown(ind = 0) {
+    let n = this.size() - 1
+    while (ind * 2 + 1 <= n) {
+      let tmp = ind
+      if (this.data[tmp] < this.data[ind * 2 + 1]) tmp = ind * 2 + 1
+      if (ind * 2 + 2 <= n && this.data[tmp] < this.data[ind * 2 + 2])
+      tmp = ind * 2 + 2
+      if (tmp === ind) break
+      this.data[tmp] = [this.data[ind], (this.data[ind] = this.data[tmp])][0]
+      ind = tmp
+    }
+  }
+  // 向上调整
+  shiftUp(idx) {
+    let pIdx = null
+    while ((pIdx = Math.floor((idx - 1) / 2)), idx && this.data[pIdx] < this.data[idx]) {
+      this.data[pIdx] = [this.data[idx], (this.data[idx] = this.data[pIdx])][0]
+      idx = pIdx
+    }
+  }
+  push(val) {
+    this.data.push(val)
+    this.shiftUp(this.size() - 1)
+  }
+  pop() {
+    if (this.size() === 0) return
+    if (this.size() === 1) {
+        return this.data.pop()
+    }
+    this.data[0] = this.data.pop()
+    this.shiftDown(0)
+  }
+  top() {
+    return this.data[0]
+  }
+  size() {
+      return this.data.length
+  }
 }
+var getLeastNumbers = function(arr, k) {
+    let h = new Heap()
+    for (let val of arr) {
+        h.push(val)
+        if (h.size() > k) h.pop()
+    }
+    return h.data
+    // arr = arr.sort((a, b) => a - b)
+    // arr = arr.slice(0, k)
+    // return arr
+};
